d7fa8d370ebd6af207f4a7a23a1e1c3dfe3fd179
Step1017 <s.aveta@yahoo.com>
2023-03-31 10:56:10 +0200

    Fatta delete

--- app/Http/Controllers/Admin/TypeController.php
+++ app/Http/Controllers/Admin/TypeController.php
@@ -96,6 +96,7 @@ class TypeController extends Controller
      */
     public function destroy(Type $type)
     {
-        //
+        $type->delete();
+        return redirect()->route('admin.types.index')->with('success', 'Tipologia eliminata con successo!');
     }
 }
